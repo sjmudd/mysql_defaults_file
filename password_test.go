64592c61226aa0ea1a8e56754465801e41af834a
Simon J Mudd <sjmudd@pobox.com>
2020-11-10 19:49:43 +0100

    Adjust test

--- password_test.go
+++ password_test.go
@@ -5,9 +5,9 @@ import (
 )
 
 type userPasswordHost struct {
+	host     string
 	user     string
 	password string
-	host     string
 }
 
 // check the different quoted passwords work
@@ -18,14 +18,16 @@ func TestPassword(t *testing.T) {
 		"testdata/my3.ini",
 	}
 	testInfo := []userPasswordHost{
-		{"root", "testpassword1", "127.0.0.1"},
-		{"root", "testpassword2", "127.0.0.1"},
-		{"root", "testpassword3", "127.0.0.1"},
+		{"127.0.0.1", "root", "testpassword1"},
+		{"127.0.0.1", "root", "testpassword2"},
+		{"127.0.0.1", "root", "testpassword3"},
 	}
 
 	for i, _ := range testIniFiles {
 		components := defaultsFileComponents(testIniFiles[i])
-		if components["host"] != testInfo[i].host || components["user"] != testInfo[i].user || components["password"] != testInfo[i].password {
+		if components["host"] != testInfo[i].host ||
+			components["user"] != testInfo[i].user ||
+			components["password"] != testInfo[i].password {
 			t.Errorf("mismatched data for %v (host: %v, user: %v, password: %q, expected: host: %v, user: %v, password: %q)",
 				testIniFiles[i],
 				components["host"],
